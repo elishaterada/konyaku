2e7f42bdc03cd51f29b9b4110534d04c0bb236f9
Elisha Terada <elishaterada@gmail.com>
2013-08-20 23:46:41 -0700

    Added RoyalSlider Added more content

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 jQuery('.button').each(function(){
 
-      jQuery(this).click(function(e){
+    // Smooth Scroll
+
+    jQuery(this).click(function(e){
         var href = jQuery(this).attr('href'),
         offsetTop = href === '#' ? 0 : jQuery(href).offset().top;
         noScroll = true;
@@ -14,6 +16,29 @@ jQuery('.button').each(function(){
             }
         });
         e.preventDefault();
+    });
+
+    // RoyalSlider
+
+    jQuery('#travel-slider').royalSlider({
+        arrowsNav: true,
+        arrowsNavAutoHide: false,
+        fadeinLoadedSlide: true,
+        controlNavigation: 'none',
+        imageScaleMode: 'fill',
+        imageAlignCenter:true,
+        loop: false,
+        loopRewind: false,
+        numImagesToPreload: 4,
+        slidesOrientation: 'vertical',
+        keyboardNavEnabled: true,
+        video: {
+            autoHideArrows:true,
+            autoHideControlNav:true
+        },
+        /* size of all images http://help.dimsemenov.com/kb/royalslider-jquery-plugin-faq/adding-width-and-height-properties-to-images */
+        imgWidth: '100%',
+        imgHeight: 360
     });
 
 });
