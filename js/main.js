583e3d8373f36d13c8ed7588435b994cd4836bee
Elisha Terada <elishaterada@gmail.com>
2013-10-24 01:39:57 -0700

    Updated jQuery UI Library Added song lyric Added tooltip translation

--- js/main.js
+++ js/main.js
@@ -1,21 +1,69 @@
-jQuery('.button').each(function(){
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+jQuery(document).ready(function() {
 
     // Smooth Scroll
 
-    jQuery(this).click(function(e){
-        var href = jQuery(this).attr('href'),
-        offsetTop = href === '#' ? 0 : jQuery(href).offset().top;
-        noScroll = true;
-        jQuery('html, body').stop().animate({
-            scrollTop: offsetTop
-        },{
-            duration: 1000,
-            easing: 'easeInOutExpo',
-            complete: function() {
-                setTimeout(function(){ noScroll = false; }, 10);
-            }
+    jQuery('.button.next').each(function(){
+
+        jQuery(this).click(function(e){
+            var href = jQuery(this).attr('href'),
+            offsetTop = href === '#' ? 0 : jQuery(href).offset().top;
+            noScroll = true;
+            jQuery('html, body').stop().animate({
+                scrollTop: offsetTop
+            },{
+                duration: 1000,
+                easing: 'easeInOutExpo',
+                complete: function() {
+                    setTimeout(function(){ noScroll = false; }, 10);
+                }
+            });
+            e.preventDefault();
         });
-        e.preventDefault();
+
+    });
+
+    // Memorials
+
+    var date_first_kiss = new Date(2005, 10, 22);
+    var date_valentines_day = new Date(2006, 02, 14);
+    var date_going_out = new Date(2008, 10, 24);
+    var today = new Date();
+
+    var since_first_kiss = Math.round((today - date_first_kiss) / (1000 * 60 * 60 * 24));
+    var since_first_kiss_comma = numberWithCommas(since_first_kiss);
+
+    var since_going_out = Math.round((today - date_going_out) / (1000 * 60 * 60 * 24));
+    var since_going_out_comma = numberWithCommas(since_going_out);
+
+    jQuery('#since-first-kiss').text(since_first_kiss_comma);
+    jQuery('#since-going-out').text(since_going_out_comma);
+
+    // RoyalSlider
+
+    jQuery('#travel-slider').royalSlider({
+        autoPlay: {
+            // autoplay options go gere
+            enabled: true,
+            pauseOnHover: true
+        },
+        arrowsNav: true,
+        arrowsNavAutoHide: false,
+        fadeinLoadedSlide: true,
+        controlNavigation: 'none',
+        imageScaleMode: 'fill',
+        imageAlignCenter: true,
+        loop: false,
+        loopRewind: false,
+        numImagesToPreload: 4,
+        slidesOrientation: 'vertical',
+        keyboardNavEnabled: true,
+        /* size of all images http://help.dimsemenov.com/kb/royalslider-jquery-plugin-faq/adding-width-and-height-properties-to-images */
+        imgWidth: 545,
+        imgHeight: 545
     });
 
     // Answer No Trick
@@ -43,28 +91,8 @@ jQuery('.button').each(function(){
         return false;
     });
 
-    // RoyalSlider
+    // Translation
 
-    jQuery('#travel-slider').royalSlider({
-        autoPlay: {
-            // autoplay options go gere
-            enabled: true,
-            pauseOnHover: true
-        },
-        arrowsNav: true,
-        arrowsNavAutoHide: false,
-        fadeinLoadedSlide: true,
-        controlNavigation: 'none',
-        imageScaleMode: 'fill',
-        imageAlignCenter: true,
-        loop: false,
-        loopRewind: false,
-        numImagesToPreload: 4,
-        slidesOrientation: 'vertical',
-        keyboardNavEnabled: true,
-        /* size of all images http://help.dimsemenov.com/kb/royalslider-jquery-plugin-faq/adding-width-and-height-properties-to-images */
-        imgWidth: 545,
-        imgHeight: 545
-    });
+    jQuery('.lyric').tooltip();
 
 });
